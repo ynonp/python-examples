4e250221542cafe098ff30fcef4a10c96d1267d4
ynonp <ynonperek@gmail.com>
2016-07-05 22:23:32 +0300

    Create 07.py

---
+++ 05_syntax_lab/07.py
@@ -0,0 +1,5 @@
+""" Write a program that selects a random number
+and asks the user to guess it.
+After each guess print a hint "too large" or "too small" to the user.
+Bonus: To make things interesting, the program should cheat once in a white
+"""
